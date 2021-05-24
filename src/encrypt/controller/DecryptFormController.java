e1eb5419d3dc96af556c994a937608fd0db59c3c
Dinusha Jayawardena <dinusha.g.jayawardena@gmail.com>
2021-05-24 16:44:57 +0530

    Sixth commit - built basic encryption method

--- src/encrypt/controller/DecryptFormController.java
+++ src/encrypt/controller/DecryptFormController.java
@@ -5,13 +5,14 @@ import javafx.scene.control.*;
 
 public class DecryptFormController {
     public Label lblOutput;
-    public Button btnConvert;
     public TextField txtInput;
     public TextField txtKey;
+    public Button btnDecrypt;
+    public TextField txtDecryptedText;
 
-    public void btnConvert_OnAction(ActionEvent actionEvent) {
+/*    public void btnConvert_OnAction(ActionEvent actionEvent) {
 
-        lblOutput.setText("");
+        *//*lblOutput.setText("");
 
         String text = txtInput.getText();
         String key = txtKey.getText();
@@ -28,7 +29,45 @@ public class DecryptFormController {
             return;
         }
 
-    //    lblOutput.setText(lblOutput.getText()+ "  " + originalText);
+    //    lblOutput.setText(lblOutput.getText()+ "  " + originalText);*//*
+
+    }*/
+
+    public void btnDecrypt_OnAction(ActionEvent actionEvent) {
+
+        String textToBeConverted = "";
+        String key;
+
+        textToBeConverted = txtInput.getText();
+        key = txtKey.getText();
+
+        if (textToBeConverted.trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR, "Please give a text convert.",ButtonType.OK).show();
+            return;
+        }
+
+        if (key.trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR, "Please give a Key.",ButtonType.OK).show();
+            return;
+        }
+
+        char[] convertedText = new char[textToBeConverted.length()];
+        String even = "";
+        String odd = "";
+        int firstEnd = 0;
+
+        for (int i = 0; i < (textToBeConverted.length() + 1)/2; i++) {
+            even += textToBeConverted.charAt(i);
+            firstEnd = i;
+        }
+
+        for (int i = firstEnd +1 ; i < textToBeConverted.length() ; i++) {
+            odd += textToBeConverted.charAt(i);
+        }
+
+        System.out.println(even);
+        System.out.println(odd);
+
 
     }
 }
